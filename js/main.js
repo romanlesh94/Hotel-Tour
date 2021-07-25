3905efd78c709c36b240a0be4d64de49eb321125
romanlesh94 <romanlesh94@gmail.com>
2021-07-25 20:39:16 +0300

    create: reviews

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper(".hotel-slider", {
     // Optional parameters
     loop: true,
   
@@ -9,7 +9,23 @@ const swiper = new Swiper('.swiper-container', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
     },
   });
